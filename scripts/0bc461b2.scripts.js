5d2951cb9b3a68b8a3db7e8a814b5605ad3408b0
Bogdan Gradinariu <bogdan.gradinariu@xivic.com>
2014-01-23 13:10:20 +0200

    Initial commit.

---
+++ scripts/0bc461b2.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("buildControlTestAppApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("buildControlTestAppApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
