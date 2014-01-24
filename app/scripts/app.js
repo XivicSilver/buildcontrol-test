35af3c1e0091cbb0ae713af495e669021c60e455
Bogdan Gradinariu <bogdan.gradinariu@xivic.com>
2014-01-24 16:47:16 +0200

    pushed yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('buildControlTestAppApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('buildControlTestAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'angular rocks!!!!',
+      'try again'
+    ];
+  });
